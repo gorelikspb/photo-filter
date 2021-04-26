296385706bfb1672521a052e688a400e7ed5bf07
Alexander Gorelik <gorelikspb@ya.ru>
2021-04-26 21:38:06 +0300

    feat: updating css vars with JS

---
+++ photo-filter/script.js
@@ -0,0 +1,12 @@
+const inputs = document.querySelectorAll('.filters input')
+
+function handleUpdate(){
+  // console.log(this.value);
+  const suffix = this.dataset.sizing || '';
+  console.log(this.name);
+
+  document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
+}
+
+inputs.forEach(input => input.addEventListener('change', handleUpdate))
+inputs.forEach(input => input.addEventListener('mousemove', handleUpdate))

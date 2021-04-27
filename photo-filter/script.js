f128348fd247c8e46fe2de1329daf4fd602b0e90
Alexander Gorelik <gorelikspb@ya.ru>
2021-04-27 17:07:47 +0300

    feat: reset btn resets img filters and outputs

--- photo-filter/script.js
+++ photo-filter/script.js
@@ -1,7 +1,8 @@
 const inputs = document.querySelectorAll('.filters input')
 
 function handleUpdate(){
-  console.log(this.name);
+
+  console.log(this);
   const inp = document.getElementsByName(this.name);
   // console.log(inp[0]);
   out = (inp[0].nextElementSibling);
@@ -14,19 +15,24 @@ function handleUpdate(){
 }
 
 inputs.forEach(input => input.addEventListener('change', handleUpdate));
-inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
+// inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
 
 const BTN_RESET = document.querySelector('.btn-reset');
-console.log(BTN_RESET);
+// console.log(BTN_RESET);
+const IMG = document.querySelector('img');
+console.log(IMG);
 
 BTN_RESET.onclick = function(){
-  inputs.forEach(input => input.value = input.defaultValue);
-  
-  // document.getElementById('myform').reset();
-  // console.log(document.getElementsByClassName('filters')[0]);
-  // document.getElementsByClassName('filters')[0].reset();
+  //TODO: DRY
+  inputs.forEach(input => {
+      input.value = input.defaultValue;
+      const inp = document.getElementsByName(input.name);
+      out = (inp[0].nextElementSibling);
+      let suff = input.dataset.sizing || '';
+      document.documentElement.style.setProperty(`--${input.name}`, input.value + suff);
+      out.value = input.value;
+    }
+  );
 
-  // document.getElementById('bodySlider').value = 0;
-  // document.getElementById('leftArmSlider').value = -20;
-  // .... etc
+
 };

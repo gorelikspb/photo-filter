759dab406aaae65e22ed20652b43bc4ca473f9df
Alexander Gorelik <gorelikspb@ya.ru>
2021-04-27 14:34:46 +0300

    feat: output values changing

--- photo-filter/script.js
+++ photo-filter/script.js
@@ -1,12 +1,31 @@
 const inputs = document.querySelectorAll('.filters input')
 
 function handleUpdate(){
-  // console.log(this.value);
-  const suffix = this.dataset.sizing || '';
   console.log(this.name);
+  const inp = document.getElementsByName(this.name);
+  // console.log(inp[0]);
+  out = (inp[0].nextElementSibling);
 
-  document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
+  const suffix = this.dataset.sizing || '';
+    document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
+    out.value = this.value;
+    // console.log (document.documentElement)
+    //TODO: add output value
 }
 
-inputs.forEach(input => input.addEventListener('change', handleUpdate))
-inputs.forEach(input => input.addEventListener('mousemove', handleUpdate))
+inputs.forEach(input => input.addEventListener('change', handleUpdate));
+inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
+
+const BTN_RESET = document.querySelector('.btn-reset');
+console.log(BTN_RESET);
+
+BTN_RESET.onclick = function(){
+  // inputs.forEach(input => input.value = );
+  // document.getElementById('myform').reset();
+  console.log(document.getElementsByClassName('filters')[0]);
+  document.getElementsByClassName('filters')[0].reset();
+
+  // document.getElementById('bodySlider').value = 0;
+  // document.getElementById('leftArmSlider').value = -20;
+  // .... etc
+};

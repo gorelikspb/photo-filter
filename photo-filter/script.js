ade1826f302eb335ffad0e071be0249842228aa9
Alexander Gorelik <gorelikspb@ya.ru>
2021-04-28 18:29:12 +0300

    feat: next btn consider daytime

--- photo-filter/script.js
+++ photo-filter/script.js
@@ -39,10 +39,23 @@ BTN_RESET.onclick = function(){
 
 /////////////
 
-const base = 'https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/evening/';
+const base = 'https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/';
+
+const date = new Date();
+let hours = date.getHours();
+
+let timeofday = 'evening'
+
+if (hours < 6) {
+  timeofday = 'night';
+} else if (hours < 12) {
+  timeofday = 'morning';
+} else if (hours <18) {
+  timeofday = 'day';
+}
+
 const images = ['01.jpg', '02.jpg', '03.jpg', '05.jpg', '06.jpg', '07.jpg', '08.jpg', '09.jpg', '10.jpg', '11.jpg', '12.jpg', '13.jpg', '14.jpg', '15.jpg', '16.jpg', '17.jpg', '18.jpg', '19.jpg', '20.jpg'];
 let i = 0;
-// const body = document.querySelector('body');
 const btn = document.querySelector('.btn-next');
 
 function viewBgImage(src) {  
@@ -55,10 +68,11 @@ function viewBgImage(src) {
 
 function getImage() {
   const index = i % images.length;
-  const imageSrc = base + images[index];
+  const imageSrc = base + timeofday + '/' + images[index];
   viewBgImage(imageSrc);
   i++;
   btn.disabled = true;
   setTimeout(function() { btn.disabled = false }, 100);
 } 
+
 btn.addEventListener('click', getImage);
